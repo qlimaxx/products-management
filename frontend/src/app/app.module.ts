c4a631369206248d8743e68cab95b524129897f2
Samir Allous <samirxallous@gmail.com>
2019-08-31 18:35:03 +0100

    Add bootstrap and ng-select in Angular app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgSelectModule,
+    FormsModule,
     HttpClientModule,
   ],
   providers: [],
