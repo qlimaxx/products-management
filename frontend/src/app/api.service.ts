ab4493ee2d4d96ad44e8153bbd8a47d1719a5261
Samir Allous <samirxallous@gmail.com>
2019-08-31 18:27:45 +0100

    Create api service in Angular app to communicate with the API gateway

---
+++ frontend/src/app/api.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  baseURL = 'http://localhost:5000/';
+
+  headers = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'X-API-Key': 'testkey'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getCategoriesWithProducts(): Observable<any> {
+    return this.http.get(this.baseURL + 'categories', {
+      params: new HttpParams().set('products', '1'),
+      headers: this.headers.headers
+    });
+  }
+
+  getCategories(): Observable<any> {
+    return this.http.get(this.baseURL + 'categories', this.headers);
+  }
+
+  getCategoryWithProducts(id: string): Observable<any> {
+    return this.http.get(this.baseURL + 'categories/' + id, {
+      params: new HttpParams().set('products', '1'),
+      headers: this.headers.headers
+    });
+  }
+
+  getCategory(id: string): Observable<any> {
+    return this.http.get(this.baseURL + 'categories/' + id, this.headers);
+  }
+
+  createCategory(data: {}): Observable<any> {
+    return this.http.post(this.baseURL + 'categories', data, this.headers);
+  }
+
+  updateCategory(id: string, data: {}): Observable<any> {
+    return this.http.put(this.baseURL + 'categories/' + id, data, this.headers);
+  }
+
+  deleteCategory(id: string): Observable<any> {
+    return this.http.delete(this.baseURL + 'categories/' + id, this.headers);
+  }
+
+  getProductsWithCategories(): Observable<any> {
+    return this.http.get(this.baseURL + 'products', {
+      params: new HttpParams().set('categories', '1'),
+      headers: this.headers.headers
+    });
+  }
+
+  getProducts(): Observable<any> {
+    return this.http.get(this.baseURL + 'products', this.headers);
+  }
+
+  getProductWithCategories(id: string): Observable<any> {
+    return this.http.get(this.baseURL + 'products/' + id, {
+      params: new HttpParams().set('categories', '1'),
+      headers: this.headers.headers
+    });
+  }
+
+  getProduct(id: string): Observable<any> {
+    return this.http.get(this.baseURL + 'products/' + id, this.headers);
+  }
+
+  createProduct(data: {}): Observable<any> {
+    return this.http.post(this.baseURL + 'products', data, this.headers);
+  }
+
+  updateProduct(id: string, data: {}): Observable<any> {
+    return this.http.put(this.baseURL + 'products/' + id, data, this.headers);
+  }
+
+  deleteProduct(id: string): Observable<any> {
+    return this.http.delete(this.baseURL + 'products/' + id, this.headers);
+  }
+
+}
