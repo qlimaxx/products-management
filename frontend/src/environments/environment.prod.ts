e685f5f0ee4193ab9dedbe9c4eb3d7f20f30e195
Samir Allous <samirxallous@gmail.com>
2019-09-01 16:38:51 +0100

    Use envs files in docker compose Change API key in Angular app

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiURL: 'http://localhost:5000/',
-  apiKey: 'testkey'
+  apiKey: 'f3277156b4241a72bf13bb894843d2b0'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiURL: 'http://localhost:5000/',
-  apiKey: 'testkey'
+  apiKey: 'f3277156b4241a72bf13bb894843d2b0'
 };
 
 /*
